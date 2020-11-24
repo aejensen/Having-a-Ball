1d26ed639c964a5c004e7b2c30f6009a210ddbeb
ekstroem <github@ekstroem.com>
2020-11-24 02:13:05 +0100

    Skal tage mig sammen

--- R/plot_finalPlayoff.R
+++ R/plot_finalPlayoff.R
@@ -14,7 +14,7 @@ par(mfrow=c(1,3), bty="n", mar = c(2.7, 2.7, 1, 0), mgp=c(1.6,0.4,0),
 
 plot(d$time, d$scorediff, pch = 19, xlab="Game time [minutes]", ylab="f | Y", 
      type="n", ylim=c(0, 40), xlim=c(0,48), xaxt="n")
-title("Running socre difference", font.main=1)
+title("Running score difference", font.main=1)
 axis(1, seq(0, 48, length.out=9))
 band(post_summary$tPred, post_summary$f_99_L, post_summary$f_99_U, col="gray85")
 band(post_summary$tPred, post_summary$f_95_L, post_summary$f_95_U, col="gray65")
@@ -28,7 +28,7 @@ legend("topleft", c("Mean", "50%", "95%", "99%"),
 
 plot(post_summary$tPred, post_summary$df_mean, ylim=c(-5, 5), type="n", lwd=2,
 		 xlab="Game time [minutes]", ylab="df | Y", yaxt="n", xaxt="n")
-title("Derivative of running socre difference", font.main=1)
+title("Derivative of running score difference", font.main=1)
 band(post_summary$tPred, post_summary$df_99_L, post_summary$df_99_U, col="gray85")
 band(post_summary$tPred, post_summary$df_95_L, post_summary$df_95_U, col="gray65")
 band(post_summary$tPred, post_summary$df_50_L, post_summary$df_50_U, col="gray45")
@@ -50,3 +50,26 @@ axis(1, seq(0, 48, length.out=9))
 dev.off()
 
 
+
+## Also make a raw plot
+
+
+
+pdf("figures/fig1-raw.pdf", width = 8, height = 5)
+
+plot(d$time, d$scorediff, pch = 19, xlab="Game time [minutes]", ylab="Score difference", 
+     type="n", ylim=c(0, 40), xlim=c(0,48), xaxt="n")
+title("Running score difference", font.main=1)
+axis(1, seq(0, 48, length.out=9))
+#band(post_summary$tPred, post_summary$f_99_L, post_summary$f_99_U, col="gray85")
+#band(post_summary$tPred, post_summary$f_95_L, post_summary$f_95_U, col="gray65")
+#band(post_summary$tPred, post_summary$f_50_L, post_summary$f_50_U, col="gray45")
+#lines(post_summary$tPred, post_summary$f_mean, lwd=2)
+points(d$time, d$scorediff, pch=19, col="black", cex=0.6)
+#legend("topleft", c("Mean", "50%", "95%", "99%"), 
+#       col = c("black", "gray45", "gray65", "gray85"), 
+#       lwd = 2, bty="n", cex=0.8, lty = c(1, NA, NA, NA), 
+#       pch = c(NA, 15, 15, 15), pt.cex=1.5)
+
+
+dev.off()
